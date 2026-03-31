491ae2434b57e3e52d64af0e46a74c5e56f61df0
NSUBUGA EDIGER <nsubugaediger@gmail.com>
2026-03-31 07:40:36 +0000

    adding the librarian in the database

---
+++ Library_management_system/app/seed.py
@@ -0,0 +1,31 @@
+from sqlachemy.orm import Session
+from database import SessionLocal, engine
+import models, security,schemas
+
+
+def seed_librarian():
+    db = SessionLocal()
+    try:
+        existing = db.query(models.Librarian).filter(models.Librarian.email == "admin@library.com").first()
+        if existing:
+            print("Librarian already exists")
+            return
+            hashed_password = security.get_password_hash("admin123")
+            new_librarian = models.Librarian(
+                full_name="Balibaseka Deogiracious",
+                email="deolee@library.com",
+                hashed_password=hashed_password,
+                is_active=True,
+                is_admin=True
+            )
+            db.add(new_librarian)
+            db.commit()
+            print("Librarian added successfully")
+        except Exception as e:
+            print(f"Error seeding database: {e}")
+        finally:
+            db.close()
+
+
+        if __name__ == "__main__":
+            seed_librarian()
