78c9cb0cf03744bc8e6005ba12ceeb8d8553517d
NSUBUGA EDIGER <nsubugaediger@gmail.com>
2026-03-31 14:25:14 +0000

    adding a librarian in the database

--- Library_management_system/app/seed.py
+++ Library_management_system/app/seed.py
@@ -1,31 +1,33 @@
-from sqlachemy.orm import Session
-from database import SessionLocal, engine
-import models, security,schemas
+from app.database import Base, SessionLocal, engine
+import app.models as models
+import app.security as security
 
 
 def seed_librarian():
+    Base.metadata.create_all(bind=engine)
     db = SessionLocal()
     try:
         existing = db.query(models.Librarian).filter(models.Librarian.email == "admin@library.com").first()
         if existing:
             print("Librarian already exists")
             return
-            hashed_password = security.get_password_hash("admin123")
-            new_librarian = models.Librarian(
-                full_name="Balibaseka Deogiracious",
-                email="deolee@library.com",
-                hashed_password=hashed_password,
-                is_active=True,
-                is_admin=True
-            )
-            db.add(new_librarian)
-            db.commit()
-            print("Librarian added successfully")
+
+        hashed_password = security.hash_password("admin123")
+        new_librarian = models.Librarian(
+            full_name="Balibaseka Deogiracious",
+            email="admin@library.com",
+            hashed_password=hashed_password,
+            is_active=True,
+            is_admin=True,
+        )
+        db.add(new_librarian)
+        db.commit()
+        print("Librarian added successfully")
     except Exception as e:
         print(f"Error seeding database: {e}")
     finally:
         db.close()
 
 
-        if __name__ == "__main__":
-            seed_librarian()
+if __name__ == "__main__":
+    seed_librarian()
